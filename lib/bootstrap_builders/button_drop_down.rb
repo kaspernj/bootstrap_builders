6f6c8e3e74e3443028f6f563f014d4bbd6fda2b4
kaspernj <kaspernj@gmail.com>
2017-10-05 14:01:41 +0000

    Added a lot more button drop down options

--- lib/bootstrap_builders/button_drop_down.rb
+++ lib/bootstrap_builders/button_drop_down.rb
@@ -1,8 +1,14 @@
 class BootstrapBuilders::ButtonDropDown
   attr_accessor :view_context
 
-  def initialize(args)
-    @args = args
+  def initialize(*args)
+    args_parser = BootstrapBuilders::ArgumentsParser.new(
+      arguments: args,
+      short_true_arguments: [:block, :danger, :link, :info, :primary, :sm, :warning]
+    )
+
+    args_parser.arguments_hash[:label] ||= args.shift if args.first.is_a?(String)
+    @args = args_parser.arguments_hash
     @buttons = []
   end
 
@@ -32,7 +38,7 @@ class BootstrapBuilders::ButtonDropDown
         "aria-exapended" => false,
         type: "button"
       },
-      classes: ["btn", "btn-default", "dropdown-toggle"],
+      classes: classes.classes,
       data: {
         toggle: "dropdown"
       }
@@ -62,5 +68,35 @@ class BootstrapBuilders::ButtonDropDown
     end
 
     btn_group.html
+  end
+
+private
+
+  def add_default_as_default
+    @classes.add("btn-default") if !@args[:danger] && !@args[:info] && !@args[:primary] && !@args[:warning]
+  end
+
+  def classes
+    unless @classes
+      @classes = BootstrapBuilders::ClassAttributeHandler.new(class: ["btn", "btn-default", "dropdown-toggle"])
+      @classes.add(@class) if @class
+      @classes.add("bb-btn-responsive") if @args[:responsive]
+      add_default_as_default
+      @classes.add("btn-block") if @args[:block]
+      @classes.add("btn-danger") if @args[:danger]
+      @classes.add("btn-info") if @args[:info]
+      @classes.add("btn-link") if @args[:link]
+      @classes.add("btn-primary") if @args[:primary]
+      @classes.add("btn-warning") if @args[:warning]
+
+      size_classes = [:lg, :md, :sm, :xs]
+      size_classes.each do |size_class|
+        next unless @args[size_class]
+        btn_size_class = "btn-#{size_class}"
+        @classes.add(btn_size_class) unless @classes.include?(btn_size_class)
+      end
+    end
+
+    @classes
   end
 end
